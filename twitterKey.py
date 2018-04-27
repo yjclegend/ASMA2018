26ebb47285d87d4a8b1d6152926fc7db4406d5e8
RegexKenny <31911769+RegexKenny@users.noreply.github.com>
2018-04-27 13:16:37 +1000

    twitter key

---
+++ twitterKey.py
@@ -0,0 +1,25 @@
+
+
+# key 1
+consumer_key1 = '1az5VnYmc4oy0faljLdSxvrzC'
+consumer_secret1 = 'NrEluXS9IGzGVru23sajuVClBXW6dbtx4MMNNO7LnLbD2otgAm'
+access_token1 = '833522486792777728-D99ckOM06s4VR4DG34YrODB2n9GQMs2'
+access_secret1 = 'UvDk8grNKrDkCl37hTJbVABCPwo3yGxr3b1o8KNLzqAuz'
+
+# key 2
+consumer_key2 = 'ViMKVl9Kj4OenOZtIDS7E8etN'
+consumer_secret2 = 'cUmmKZ3orZckNOryOaoWMdQuLbCp9zJTKhNQTSyHPsIX3Pnu00'
+access_token2 = '986821001068527616-5DrJPirExdMDEo3ENZTbQGd2nq5s7Xh'
+access_secret2 = 'EKWG50yUCS9JflmcBUoBCwEzOXaeCRhv6sDXA3MmOdQ8R'
+
+# key 3
+consumer_key3 = '2rPKn6i0H8FzEGtDflbPAnnZq'
+consumer_secret3 = '7GYVIiMluCWPNvJhZkOOX79kpeDgLx4VsDflNQqoQ8CjaqpdHT'
+access_token3 = '51425680-asx0ZUcVzJnAHX8cFnTJ5bG4QOonlnzFvZi2xBnv'
+access_secret3 = 'Y3pUl1XtZZUQn61b983ZlJhuVg5jvAieNFbLCrKbsDLH2'
+
+# key 4
+# consumer_key4 =
+# consumer_secret4 =
+# access_token4 =
+# access_secret4 =
