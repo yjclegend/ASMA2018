03df2596f3aab5c675d3ce6521ebbc71c0941d90
yjclegend <yjclegend@163.com>
2018-05-08 18:52:18 -0700

    new stable version

---
+++ deploy/credential.py
@@ -0,0 +1,5 @@
+key = 'd76c0dd861f346b0acb093220c421eb4'
+secret = 'd4acf02f362341b2ad66a7fdc43c14c6'
+size = 'm2.medium'
+
+privateKey = 'huozhua.key'
---
+++ deploy/credential_demo.py
@@ -0,0 +1,5 @@
+key = '8748abce2ae74086bbf8317f3d2d80ae'
+secret = '229e5f671b60430b9ac2573113017e33'
+size = 'm2.tiny'
+
+privateKey = 'huozhua.key'
